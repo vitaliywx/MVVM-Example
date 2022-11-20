78d6d11eadab9fbba5198aea76fe79d2f0bdfa09
vitaliywx <vilaliy.wx@gmail.com>
2022-11-20 16:17:12 +0200

    Small fix

--- MVVM-Example/ViewModels/ViewModel.swift
+++ MVVM-Example/ViewModels/ViewModel.swift
@@ -12,11 +12,13 @@ class ViewModel {
     var statusText = Dynamic("")
     var statusColor = Dynamic(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1))
     
+    var text = "You succesfully logged in."
+    
     func userButtonPressed(login: String, password: String) {
         
         for i in 0..<User.logins.count {
             if User.logins[i].login! == login && User.logins[i].password! == password {
-                statusText.value = "You succesfully logged in."
+                statusText.value = text
                 statusColor.value = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
                 print(statusText.value)
             }
